96611752dd8fe480e7a4d1c1e16c6c4b919bb4d3
moosipea <33414810+moosipea@users.noreply.github.com>
2020-05-07 22:01:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById("btn1").addEventListener("click", function(){
+    window.location.href = 'index.html'; 
+  });
+document.getElementById("btn2").addEventListener("click", function(){
+    window.location.href = 'page_webexp.html';
+});
+document.getElementById("btn3").addEventListener("click", function(){
+    window.location.href = 'page_music.html'; 
+  });
+document.getElementById("btn4").addEventListener("click", function(){
+    window.location.href = 'page_mcbuilds.html';
+});
